1e7836cc1d23c32c9b59379ae2edf1de6d4de875
Giulliano <llthguimaraes@gmail.com>
2023-04-02 17:30:59 -0300

    scroll to top effect

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,11 +1,32 @@
 const fixedHeader = document.querySelector(".j_fixed_header");
 const mobileMenu = document.querySelector(".j_mobile_menu");
 const mobileMenuIcon = document.querySelector(".j_mobile_menu_icon");
+const arrowUp = document.querySelector(".j_arrowup");
 
 // TOGGLE FIXED HEADER
 window.onscroll = () => {
-    fixedHeader.style.backgroundColor = window.scrollY > 0 ? "black" : "";
-    fixedHeader.style.padding = window.scrollY > 0 ? "10px 0" : "";
+
+    if (window.scrollY > 0) {
+        fixedHeader.style.backgroundColor = "black";
+        fixedHeader.style.padding = "10px 0";
+
+        arrowUp.style.display = "flex";
+
+        setTimeout(() => {            
+            arrowUp.style.opacity = 1;
+            arrowUp.style.bottom = "20px";
+        }, 10);
+    } else {
+        fixedHeader.style.backgroundColor = "";
+        fixedHeader.style.padding = "";
+
+        arrowUp.style.opacity = "";
+        arrowUp.style.bottom = "";
+
+        setTimeout(() => {
+            arrowUp.style.display = "";
+        }, 300);
+    }
 }
 
 // MOBILE MENU
@@ -22,4 +43,9 @@ mobileMenuIcon.addEventListener("click", () => {
         mobileMenuIcon.style.transform = "";
         mobileMenuIcon.style.opacity = "";
     }, 300);
+})
+
+// SCROLL UP WITH ARROW
+arrowUp.addEventListener("click", () => {
+    window.scrollTo(0, 0);
 })
